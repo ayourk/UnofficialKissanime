48384705bc6403e06f8ad5e1c350e395de198012
dat1guy <dat1guy54@outlook.com>
2016-08-18 00:59:32 -0700

    Added backup search in IMDB if couldn't find in TVDB.  This works for One Punch Man, but I doubt it'll work on much more.  A little bit of work for a popular show...

--- resources/lib/common/loose_metahandlers.py
+++ resources/lib/common/loose_metahandlers.py
@@ -111,21 +111,6 @@ class LooseMetaData(MetaData):
         else:
             helper.log_debug('The absolute_episode column already exists')
 
-    def _does_column_exist(self, column_name, table):
-        sql_pragma = 'PRAGMA table_info(episode_meta)'
-        try:
-            self.dbcur.execute(sql_pragma)
-            matched_rows = self.dbcur.fetchall()
-        except:
-            common.addon.log_debug('Unable to execute sql for column existance query')
-            return True
-        return ([r for r in matched_rows if r['name'] == 'absolute_episode'] != [])
-
-    def _MetaData__init_episode_meta(self, imdb_id, tvdb_id, episode_title, season, episode, air_date):
-        meta = MetaData._MetaData__init_episode_meta(self, imdb_id, tvdb_id, episode_title, season, episode, air_date)
-        meta['absolute_episode'] = 0
-        return meta
-    
     def get_episodes_meta(self, tvshowtitle, imdb_id, num_episodes, first_air_date='', season=None):
         '''
         Returns all metadata about the given number of episodes (inclusive) for
@@ -146,7 +131,7 @@ class LooseMetaData(MetaData):
                 helper.log_debug('Grabbing show and episodes for metadata')
                 tvdb = TheTVDB(language=self._MetaData__get_tvdb_language())
                 (show, episode_list) = tvdb.get_show_and_episodes(tvdb_id)
-                meta_list = [self.episode_to_meta(ep, tvshowtitle, show) for ep in episode_list]
+                meta_list = [self._episode_to_meta(ep, tvshowtitle, show) for ep in episode_list]
                 # we want to save the metadata for all the episodes (for 
                 # caching reasons), so we'll filter later
             else:
@@ -173,52 +158,21 @@ class LooseMetaData(MetaData):
         helper.end('get_episodes_meta')
         return meta_list
 
-    def __filter_meta_list_by_airdate(self, meta_list, first_air_date, num_episodes):
-        helper.log_debug('Filtering metadata list by airdate %s' % first_air_date)
-        # For some stupid reason, the tvdb may have the premiere date one-off, 
-        # maybe because it aired in Japan first or something, I don't know
-        first_date = helper.get_datetime(first_air_date, '%Y-%m-%d')
-        tmp_meta_list = []
-        for meta in meta_list:
-            if num_episodes == 0: # end of the sequence
-                break
-            if meta['absolute_episode'] == -1:
-                helper.log_debug('Filtering out meta')
-                continue
-            if len(tmp_meta_list) > 0: # middle of the sequence
-                helper.log_debug('Found next meta')
-                tmp_meta_list.append(meta)
-                num_episodes -= 1
-                continue
-            date = helper.get_datetime(meta['premiered'], '%Y-%m-%d')
-            days = abs((first_date - date).days)
-            if days <= 1: # start of the sequence
-                helper.log_debug('Found first meta')
-                tmp_meta_list.append(meta)
-                num_episodes -= 1
-            else:
-                helper.log_debug('Skipping meta %s' % str(meta))
-        return tmp_meta_list
+    def _does_column_exist(self, column_name, table):
+        sql_pragma = 'PRAGMA table_info(episode_meta)'
+        try:
+            self.dbcur.execute(sql_pragma)
+            matched_rows = self.dbcur.fetchall()
+        except:
+            common.addon.log_debug('Unable to execute sql for column existance query')
+            return True
+        return ([r for r in matched_rows if r['name'] == 'absolute_episode'] != [])
 
-    def __filter_meta_list_by_season(self, meta_list, season, num_episodes):
-        helper.log_debug('Filtering metadata list by season %d' % season)
-        tmp_meta_list = []
-        for meta in meta_list:
-            if num_episodes == 0:
-                break
-            if len(tmp_meta_list) > 0: # in sequence
-                helper.log_debug('Found next meta')
-                tmp_meta_list.append(meta)
-                num_episodes -= 1
-            elif meta['season'] == season:
-                helper.log_debug('Found first meta')
-                tmp_meta_list.append(meta)
-                num_episodes -= 1
-            else:
-                helper.log_debug('Skipping meta')
-                pass
-        return tmp_meta_list
-
+    def _MetaData__init_episode_meta(self, imdb_id, tvdb_id, episode_title, season, episode, air_date):
+        meta = MetaData._MetaData__init_episode_meta(self, imdb_id, tvdb_id, episode_title, season, episode, air_date)
+        meta['absolute_episode'] = 0
+        return meta
+    
     def _cache_lookup_episodes(self, imdb_id, tvdb_id, first_air_date, season, num_episodes):
         '''
         Lookup metadata for multiple episodes starting from the first air date
@@ -297,16 +251,14 @@ class LooseMetaData(MetaData):
         else:
             sql_select = 'SELECT * from episode_meta WHERE title = "%s"' % self._clean_string(meta_list[0]['title'].tolower())
             sql_delete = 'DELETE * from episode_meta WHERE title = "%s"' % self._clean_string(meta_list[0]['title'].tolower())
-        helper.log_debug('Saving metadata for all episodes')
         helper.log_debug('SQL Select: %s' % sql_select)
 
         try:
             self.dbcur.execute(sql_select)
             matchedrow = self.dbcur.fetchone()
             if matchedrow:
-                    helper.log_debug('Episode matched row found, deleting table entry')
-                    helper.log_debug('SQL Delete: %s' % sql_delete)
-                    self.dbcur.execute(sql_delete)
+                helper.log_debug('SQL Delete: %s' % sql_delete)
+                self.dbcur.execute(sql_delete)
         except Exception as e:
             helper.log_debug('************* Error attempting to delete episodes from cache table: %s ' % e)
             pass
@@ -354,18 +306,19 @@ class LooseMetaData(MetaData):
         try:
             if imdb_id:
                 tvdb_id = tvdb.get_show_by_imdb(imdb_id)
+                helper.log_debug('Got tvdb_id %s from imdb_id %s' % (tvdb_id, imdb_id))
         except Exception, e:
             common.addon.log('************* Error retreiving from thetvdb.com: %s ' % e, 4)
             tvdb_id = ''
             pass
             
-        #Intialize tvshow meta dictionary
+        # Intialize tvshow meta dictionary
         meta = self._init_tvshow_meta(imdb_id, tvdb_id, name, year)
 
-        # if not found by imdb, try by name
+        # If not found by imdb, try by name
         if tvdb_id == '':
             try:
-                #If year is passed in, add it to the name for better TVDB search results
+                # If year is passed in, add it to the name for better TVDB search results
                 if year:
                     name = name + ' ' + year
                 show_list = tvdb.get_matching_shows(name, want_raw=True)
@@ -373,25 +326,23 @@ class LooseMetaData(MetaData):
                 common.addon.log('************* Error retreiving from thetvdb.com: %s ' % e, 4)
                 show_list = []
                 pass
-            common.addon.log('Found TV Show List: %s' % show_list, 0)
-            tvdb_id=''
-            prob_id=''
+            tvdb_id = ''
+            prob_id = ''
 
-            helper.log_debug('Here are the raw results: %s' % str(show_list))
+            helper.log_debug('Raw results of tv show candidates: %s' % str(show_list))
             strcmp = self._string_compare
             clean = self._clean_string
             for show in show_list:
-                # this is probably our result
-                tmp_imdb_id = show['IMDB_ID'] if show.has_key('IMDB_ID') else None
-                if tmp_imdb_id == imdb_id or strcmp(clean(show['SeriesName']), clean(name)) or len(show_list) == 1: # if the list length is one, then this is probably the right result
+                # This is probably our result (or if the list length is 1!)
+                tmp_imdb_id = show.get('IMDB_ID', None)
+                if tmp_imdb_id == imdb_id or strcmp(clean(show['SeriesName']), clean(name)) or len(show_list) == 1:
                     tvdb_id = clean(show['seriesid'])
                     if not imdb_id:
                         imdb_id = clean(tmp_imdb_id)
                     break
                 # Check aliases
                 if show.has_key('AliasNames'):
-                    helper.log_debug('looking at AliasNames: ' + show['AliasNames'].encode('ascii', errors='xmlcharrefreplace'))
-                    helper.log_debug('here it is split: %s' % str(show['AliasNames'].encode('ascii', errors='xmlcharrefreplace').split('|')))
+                    helper.log_debug('Looking at the aliases: %s' % str(show['AliasNames'].encode('ascii', errors='xmlcharrefreplace').split('|')))
                     for alias in show['AliasNames'].split('|'):
                         if strcmp(clean(alias), clean(name)):
                             prob_id = clean(show['seriesid'])
@@ -399,6 +350,16 @@ class LooseMetaData(MetaData):
                                 imdb_id = clean(tmp_imdb_id)
             if tvdb_id == '' and prob_id != '':
                 tvdb_id = self._clean_string(prob_id)
+
+
+        # If we still haven't found anything, let's try IMDB to see if we can find a show
+        if tvdb_id == '' and imdb_id == '':
+            tmdb = TMDB(api_key=self.tmdb_api_key, lang=self._MetaData__get_tmdb_language())
+            imdb_meta = tmdb.search_imdb(name)
+            if imdb_meta and 'tt' in imdb_meta.get('imdbID', ''):
+                imdb_id = imdb_meta.get('imdbID')
+                helper.log_debug('Found metadata from IMDB with id %s! Restarting _get_tvdb_meta' % imdb_id)
+                return self._get_tvdb_meta(imdb_id, name, year)
 
         if tvdb_id:
             common.addon.log('Show *** ' + name + ' *** found in TVdb. Getting details...', 0)
@@ -411,39 +372,10 @@ class LooseMetaData(MetaData):
                 pass
             
             if show is not None:
-                meta['imdb_id'] = imdb_id
-                meta['tvdb_id'] = tvdb_id
-                meta['title'] = name
-                if str(show.rating) != '' and show.rating != None:
-                    meta['rating'] = float(show.rating)
-                meta['duration'] = int(show.runtime) * 60
-                meta['plot'] = show.overview
-                meta['mpaa'] = show.content_rating
-                meta['premiered'] = str(show.first_aired)
-
-                #Do whatever we can to set a year, if we don't have one lets try to strip it from show.first_aired/premiered
-                if not year and show.first_aired:
-                        #meta['year'] = int(self._convert_date(meta['premiered'], '%Y-%m-%d', '%Y'))
-                        meta['year'] = int(meta['premiered'][:4])
-
-                if show.genre != '':
-                    temp = show.genre.replace("|",",")
-                    temp = temp[1:(len(temp)-1)]
-                    meta['genre'] = temp
-                meta['studio'] = show.network
-                meta['status'] = show.status
-                if show.actors:
-                    for actor in show.actors:
-                        meta['cast'].append(actor)
-                meta['banner_url'] = show.banner_url
-                meta['imgs_prepacked'] = self.prepack_images
-                meta['cover_url'] = show.poster_url
-                meta['backdrop_url'] = show.fanart_url
-                meta['overlay'] = 6
-
+                helper.log_debug('Got a show from tvdb_id %s' % str(show))
+                meta = self._show_to_meta(show, imdb_id, tvdb_id, name, year)
                 if meta['plot'] == 'None' or meta['plot'] == '' or meta['plot'] == 'TBD' or meta['plot'] == 'No overview found.' or meta['rating'] == 0 or meta['duration'] == 0 or meta['cover_url'] == '':
                     common.addon.log(' Some info missing in TVdb for TVshow *** '+ name + ' ***. Will search imdb for more', 0)
-                    helper.log_debug('help me please %s' % str(dir(self)))
                     tmdb = TMDB(api_key=self.tmdb_api_key, lang=self._MetaData__get_tmdb_language())
                     imdb_meta = tmdb.search_imdb(name, imdb_id)
                     if imdb_meta:
@@ -469,7 +401,41 @@ class LooseMetaData(MetaData):
         else:
             return meta
 
-    def episode_to_meta(self, episode, tvshowtitle, show, overlay=6, playcount=0):
+    def _show_to_meta(self, show, imdb_id, tvdb_id, show_name, year):
+        meta = {}
+        meta['imdb_id'] = imdb_id
+        meta['tvdb_id'] = tvdb_id
+        meta['title'] = show_name
+        if str(show.rating) != '' and show.rating != None:
+            meta['rating'] = float(show.rating)
+        meta['duration'] = int(show.runtime) * 60
+        meta['plot'] = show.overview
+        meta['mpaa'] = show.content_rating
+        meta['premiered'] = str(show.first_aired)
+
+        #Do whatever we can to set a year, if we don't have one lets try to strip it from show.first_aired/premiered
+        if not year and show.first_aired:
+            meta['year'] = int(meta['premiered'][:4])
+
+        if show.genre != '':
+            temp = show.genre.replace("|",",")
+            temp = temp[1:(len(temp)-1)]
+            meta['genre'] = temp
+        meta['studio'] = show.network
+        meta['status'] = show.status
+        meta['cast'] = []
+        if show.actors:
+            for actor in show.actors:
+                meta['cast'].append(actor)
+        meta['banner_url'] = show.banner_url
+        meta['imgs_prepacked'] = self.prepack_images
+        meta['cover_url'] = show.poster_url
+        meta['backdrop_url'] = show.fanart_url
+        meta['overlay'] = 6
+        meta['trailer_url'] = ''
+        return meta
+
+    def _episode_to_meta(self, episode, tvshowtitle, show, overlay=6, playcount=0):
         meta = {}
         meta['imdb_id'] = self._check(episode.imdb_id)
         meta['tvdb_id'] = self._check(episode.show_id)
@@ -505,6 +471,52 @@ class LooseMetaData(MetaData):
         meta['banner_url'] = show.banner_url
 
         return meta
+
+    def __filter_meta_list_by_airdate(self, meta_list, first_air_date, num_episodes):
+        helper.log_debug('Filtering metadata list by airdate %s' % first_air_date)
+        # For some stupid reason, the tvdb may have the premiere date one-off, 
+        # maybe because it aired in Japan first or something, I don't know
+        first_date = helper.get_datetime(first_air_date, '%Y-%m-%d')
+        tmp_meta_list = []
+        for meta in meta_list:
+            if num_episodes == 0: # end of the sequence
+                break
+            if meta['absolute_episode'] == -1:
+                helper.log_debug('Filtering out meta')
+                continue
+            if len(tmp_meta_list) > 0: # middle of the sequence
+                helper.log_debug('Found next meta')
+                tmp_meta_list.append(meta)
+                num_episodes -= 1
+                continue
+            date = helper.get_datetime(meta['premiered'], '%Y-%m-%d')
+            days = abs((first_date - date).days)
+            if days <= 1: # start of the sequence
+                helper.log_debug('Found first meta')
+                tmp_meta_list.append(meta)
+                num_episodes -= 1
+            else:
+                helper.log_debug('Skipping meta %s' % str(meta))
+        return tmp_meta_list
+
+    def __filter_meta_list_by_season(self, meta_list, season, num_episodes):
+        helper.log_debug('Filtering metadata list by season %d' % season)
+        tmp_meta_list = []
+        for meta in meta_list:
+            if num_episodes == 0:
+                break
+            if len(tmp_meta_list) > 0: # in sequence
+                helper.log_debug('Found next meta')
+                tmp_meta_list.append(meta)
+                num_episodes -= 1
+            elif meta['season'] == season:
+                helper.log_debug('Found first meta')
+                tmp_meta_list.append(meta)
+                num_episodes -= 1
+            else:
+                helper.log_debug('Skipping meta')
+                pass
+        return tmp_meta_list
 
     def __cache_find_absolute_episode(self, tvdb_id, first_air_date, season):
         sql_select = 'SELECT absolute_episode FROM episode_meta WHERE tvdb_id=? AND '
