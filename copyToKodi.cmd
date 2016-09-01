a92ff9e0317e3b6d9637eb5123e44ec9fc11dea6
dat1guy <dat1guy54@outlook.com>
2016-09-01 03:28:35 -0700

    Cleaning up repo of extra files.

--- copyToKodi.cmd
+++
@@ -1,9 +0,0 @@
-rmdir /s /q plugin.video.unofficialkissanime
-mkdir plugin.video.unofficialkissanime
-copy *py plugin.video.unofficialkissanime
-copy *xml plugin.video.unofficialkissanime
-copy icon.png plugin.video.unofficialkissanime
-copy README.md plugin.video.unofficialkissanime
-xcopy /E resources plugin.video.unofficialkissanime\resources\
-xcopy /E /Y plugin.video.unofficialkissanime "%appdata%\kodi\addons\plugin.video.unofficialkissanime\"
-del /S /Q "%appdata%\kodi\addons\plugin.video.unofficialkissanime\*pyo"
--- createzip.cmd
+++
@@ -1,9 +0,0 @@
-del plugin.video.unofficialkissanime.zip
-rmdir /s /q plugin.video.unofficialkissanime
-mkdir plugin.video.unofficialkissanime
-copy *py plugin.video.unofficialkissanime
-copy *xml plugin.video.unofficialkissanime
-copy icon.png plugin.video.unofficialkissanime
-copy README.md plugin.video.unofficialkissanime
-xcopy /E resources plugin.video.unofficialkissanime\resources\
-"C:\Program Files\7-Zip\7z.exe" a plugin.video.unofficialkissanime-0.0.1.zip plugin.video.unofficialkissanime\
